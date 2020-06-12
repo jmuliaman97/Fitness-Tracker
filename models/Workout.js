185d7db228acce188c0d3eb3b3e518d597cc4054
Jordan M <jordan.muliaman@gmail.com>
2020-06-11 22:49:36 -0700

    create MVC paradigm

---
+++ models/Workout.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose')
+
+// module.exports = model('Person', new Schema({
+//   username: String,
+//   email: String,
+//   password: String
+// }))
+
+module.exports = model('Workout', new Schema ({
+  
+}))
